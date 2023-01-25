47a2936ee5585cc9b69a75ff4f081ba2988763b4
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-01-25 04:03:05 -0300

    lista de compras com lista

--- exercicio_8.py
+++ exercicio_8.py
@@ -1,19 +1,35 @@
 lista = []
-while True:
+x = 0
+
+while True:   
    
-    
     lista_devalores = input('[i]inserir [a]apagar [l]listar: ')
+    
+    
     if lista_devalores == 'i':
         inserindo = input('insira algo na lista: ')
-        lista.append(inserindo)
-        print(lista)
+        if inserindo.isdigit():
+            if inserindo.isalnum():
+                print('erro digite só letras')
+        elif inserindo.isalpha():
+            lista.append(inserindo)
+            print(lista)
     
-    try:
-       lista_str = str(lista_devalores) 
-    except:
-        print('digite novamente')
-        lista_devalores = None
-    if lista_devalores == None:
-        print('um ou ambos números estão nomes digitados estão errados, digite novamente')
+    elif lista_devalores == 'l':
+        for item in enumerate(lista):       
+            print(item)
+        if len(lista) == 0:
+            print('a lista esta vazia insira algo')     
+        
+    elif lista_devalores == 'a':
+        apagando = input('insira um indíce para deletar: ')
+        
 
-    
+        try:
+
+            indice = int(apagando)
+            del lista[indice]
+        except:
+            print('indice inválido')
+    else:
+        print('digite l, i ou a')
