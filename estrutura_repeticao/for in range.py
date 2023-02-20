5d34d3899a2dd83f91b3d9a3511657e2f0bb2b2f
Alan Queiroz <alanqueiroz@outlook.com>
2023-02-20 20:52:59 -0300

    criando estrutura de pastas

---
+++ estrutura_repeticao/for in range.py
@@ -0,0 +1,5 @@
+for numero in range(-11, 0):
+    print(numero)
+b = 7
+egra_de_sinal = -(b)
+print(egra_de_sinal)
--- projeto_2.py
+++ projeto_2.py
@@ -3,50 +3,46 @@ from datetime import date
 while True:
     ativo = input('QUAL É SEU ATIVO: ')
 
-    
-    
     if len(ativo) == 6 and not ativo.isnumeric() and not ativo.isalpha():
-        
+
         while True:
-                tipo = input('TIPO.DIGITE 5 ELEMENTOS COM TODOS ESSES LETRAS: ')
-                
-                if tipo.isdigit():
-                    continue
-                elif len(tipo) == 5 and tipo.isalpha():   
-                    while True:
-                
-                
-                        preco_unitario = input('PREÇO UNITÁRIO')
-                        if preco_unitario.isalpha():
-                            print('DIGITE SÓ NÚMEROS')
-                            continue    
-                        if preco_unitario.isnumeric():
-                            preco_unitario_int = float(preco_unitario)
-                        
-                            while True:
-                                quantidade = input('QUANTIDADE: ')
-                                if quantidade.isnumeric():
-                                    quantidade_int = float(quantidade)
-                                    data_atual = date.today()
-                                    valor_investido = quantidade_int * preco_unitario_int
+            tipo = input('TIPO.DIGITE 5 ELEMENTOS COM TODOS ESSES LETRAS: ')
 
-                                    
-                                    print(
-                                        'TA DO APDAORTE|  ATIVO  |  TIPO  |  PREÇO UNITÁRIO  |  QUANTIDADE  |  VALOR INVESTIDOR   |    ')
-                                    print(
-                                        f'{data_atual}       {ativo} |  {tipo}    r${preco_unitario}                  {quantidade}              r${valor_investido}')
-                                    break
-                            break
+            if tipo.isdigit():
+                continue
+            elif len(tipo) == 5 and tipo.isalpha():
+                while True:
 
-                    break
+                    preco_unitario = input('PREÇO UNITÁRIO')
+                    if preco_unitario.isalpha():
+                        print('DIGITE SÓ NÚMEROS')
+                        continue
+                    if preco_unitario.isdecimal():
+                        preco_unitario_int = float(preco_unitario)
+
+                        while True:
+                            quantidade = input('QUANTIDADE: ')
+                            if quantidade.isnumeric():
+                                quantidade_int = float(quantidade)
+                                data_atual = date.today()
+                                valor_investido = quantidade_int * preco_unitario_int
+
+                                print(
+                                    'TA DO APDAORTE|  ATIVO  |  TIPO  |  PREÇO UNITÁRIO  |  QUANTIDADE  |  VALOR INVESTIDOR   |    ')
+                                print(
+                                    f'{data_atual}       {ativo} |  {tipo}    r${preco_unitario}                  {quantidade}              r${valor_investido}')
+                                break
+                        break
+
+                break
         break
-    #else:
-        #print('DIGITE SÓ NÚMEROS')
+    # else:
+        # print('DIGITE SÓ NÚMEROS')
 
     else:
         print('ATIVO INVÁLIDO! O ATIVO DEVE TER NO MÍNIMO 5 ELEMENTOS COM LETRAS E NÚMEROS')
         continue
-    
+
 
 '''''
 tipo = input('TIPO: ')
@@ -69,5 +65,4 @@ tipo = input('TIPO: ')
                         else:
                             print('DIGITE SÓ LETRAS')
                             continue
- '''           
-
+ '''
