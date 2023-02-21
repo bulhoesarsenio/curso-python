1f9c32fe39df59d7284d67303cd844a6a78ab845
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-02-21 00:48:35 -0300

    equação do 2 grau em andamento

--- equacao_do_2_grau.py
+++ equacao_do_2_grau.py
@@ -1,61 +1,26 @@
-''''
-def valores(a, b, c):
-    print(f'{a}x² {b}x {c} = 0')
-         
-
-    print(f'{a=} |' 
-          f'{b=} |' 
-          f'{c=} |')
-    potencia = b ** 2
-    
-    expressao_numerica = -4 * a * c
-    print(f'Δ = ({b}²) - 4.{a}.({c})')
-    if c > 0:
-        print(f'Δ = {potencia} { expressao_numerica}')
-        subtracao = expressao_numerica + potencia 
-        print(f'Δ = {subtracao}')
-    else:
-        soma = potencia + expressao_numerica
-        print(f'Δ = {potencia} + {expressao_numerica}')
-        print(f'Δ = {soma}')
-        print('x = -b +- √Δ'
-              '\n-----------'
-              '\n    2.a')
-        print(f'x = -({b}) +- √{soma}')
-    
-        
-        
-while True:
-    try:
-        valores(a = int(input('digite um número para (a): ')), 
-                b= int(input('digite um número para (b): ')),
-                c = int(input('digite um número para (c): ')))
-        break
-    except:
-        print('11mNÚMERO INVÁLIDO')
-        continue
-'''
-
 import math
+from fractions import Fraction
 
 
 def valores(a, b, c):
-    print(f'{a}x² {b}x {c} = 0')
-
-    print(f'{a=} |'
-          f'{b=} |'
-          f'{c=} |')
     potencia = b ** 2
     expressao_numerica = -4 * a * c
     subtracao = expressao_numerica + potencia
-    print(f'Δ = ({b}²) - 4.{a}.({c})')
     if subtracao < 0:
-        print(f'Δ = {potencia} { expressao_numerica}')
-        print(f'Δ = {subtracao}')
+        
+        print('IMPOSSÍVEL DE CALCULAR A EQUAÇÃO, POIS A RAIZ QUADRADA É NEGATIVA ENTÃO A SOLUÇÃO É S = {} ')
+    elif a == 0:
+        print('IMPOSSÍVEL DE CALCULAR A EQUAÇÃO, POIS A RAIZ QUADRADA É NEGATIVA ENTÃO A SOLUÇÃO É S = {} ')
     else:
+        print(f'{a}x² {b}x {c} = 0')
+
+        print(f'{a=} |'
+          f'{b=} |'
+          f'{c=} |')
         soma = potencia + expressao_numerica
         print(f'Δ = {potencia} + {expressao_numerica}')
         print(f'Δ = {soma}')
+        print('')
         print('x = -b +- √Δ'
               '\n-----------'
               '\n    2.a')
@@ -68,15 +33,23 @@ def valores(a, b, c):
         print(f'x = {regra_de_sinal} +- {raiz} '
               '\n------------'
               f'\n    {divisao}')
-        print(f'x1 = {b} + {raiz}'
+        print('')
+        if b > 0: 
+
+            print(f'x1 = -{b} + {raiz}'
+               '\n------------'
+               f'\n   {divisao}')
+        if b < 0:
+              print(f'x1 = {b} + {raiz}'
                '\n------------'
                f'\n   {divisao}')
         x_1 = regra_de_sinal + raiz
+        
         print(f'x1 = {x_1}'
                '\n------------'
                f'\n   {divisao}')
         print(f'x1 = {x_1 / divisao}')
-
+        print('')
         print('=====================================')
 
         x_2 = regra_de_sinal - raiz
@@ -91,9 +64,10 @@ def valores(a, b, c):
         print(f'x2 = {x_2 / divisao}')
 
 
-        print('S = %.4f' % resultado_x1,  resultado_x2)
-
-
+        print('S x1= %.2f' % resultado_x1)
+        print('S x2= %.2f' % resultado_x2)
+        print(Fraction(resultado_x2))
+        
 while True:
     try:
         valores(a=int(input('digite um número para (a): ')),
@@ -101,5 +75,7 @@ while True:
                 c=int(input('digite um número para (c): ')))
         break
     except:
+
         print('NÚMERO INVÁLIDO')
         continue
+    
