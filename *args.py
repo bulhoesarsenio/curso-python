e9ae6e3e0a2dc7671e51dba6b8eecefb23b1febc
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-02-28 14:00:34 -0300

    argumento que recebe varios valores

---
+++ *args.py
@@ -0,0 +1,15 @@
+
+def valor(*args):
+    interando = range(len(args))
+    soma = 0
+    for c in interando:
+        soma += args[c]
+    
+    #print(f'a soma entre {args} é:', soma)
+    return soma
+
+numero = valor(10,23)
+numero_2 = valor(3, 2, 4)
+
+print(numero)
+print(numero_2)
