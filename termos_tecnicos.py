f0f7406529a97fc6c21f78d9a0218c5b64ceedc9
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-03-04 20:56:59 -0300

    termos técnicos de funções

---
+++ termos_tecnicos.py
@@ -0,0 +1,4 @@
+#higer order fuctions: funções que podem receber argumentos, ou execultar outras funções
+
+#first-class fuctions: funções que são tratadas como outros tipos de dados ex: int, str...
+
