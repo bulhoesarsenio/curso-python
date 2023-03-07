8dd6a6afcbea06cfbaceb977174614f8366b8fcf
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-03-06 22:16:09 -0300

    exercicío com função

---
+++ exercicio_12.py
@@ -0,0 +1,23 @@
+# Exercícios
+# Crie funções que duplicam, triplicam e quadruplicam
+# o número recebido como parâmetro.
+# def duplicar(numero):
+#     return numero * 2
+
+
+# def triplicar(numero):
+#     return numero * 3
+
+
+# def quadruplicar(numero):
+#     return numero * 4
+def funcao(numero):
+    return numero 
+
+duplicar = funcao(2)
+triplicar = funcao(3)
+quadruplicar = funcao(4)
+
+print((2*duplicar))
+print(triplicar * 2)
+print(quadruplicar * 2)
