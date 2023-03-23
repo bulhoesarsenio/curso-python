bda19e33a3d98987c3636ad4ab1dbba4b83c9296
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-03-22 21:38:48 -0300

    equação do segundo grau com tratamentos de errro

---
+++ metodos_uteis_dicionario.py
@@ -0,0 +1,10 @@
+dicit = {'nome': 'arsenio',
+         'idade': '14'}
+
+#nome = dicit.pop('nome') exclui um item especifíco
+
+#apagando = dicit.popitem() exclui o ultimo item
+
+#dicit.update({'nome': 'neto'}) update atualiza a lista
+
+print(dicit)
