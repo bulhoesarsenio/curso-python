89dfa70d596db3d61a04f48fbf08b62c75e78735
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-04-08 21:19:27 -0300

    função args

--- *args.py
+++ *args.py
@@ -1,12 +1,13 @@
 
 def valor(*args):
+
     interando = range(len(args))
     soma = 0
     for c in interando:
-        soma += args[c]
+       soma += args[c]
     
     #print(f'a soma entre {args} é:', soma)
-    return soma
+    #return soma
 
 numero = valor(10,23)
 numero_2 = valor(3, 2, 4)
