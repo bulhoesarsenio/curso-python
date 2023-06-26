99d329f8534b1482f5720b2dd234405fc4e20ce0
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-06-26 19:54:30 -0300

    função lambida em python

--- funcao_lambida.py
+++ funcao_lambida.py
@@ -1,12 +1,24 @@
-lista = [1,4,2,3,6]
+lista = [1, 4, 2, 3, 6]
 
 lista.sort()
 print(lista)
 
-diconario = [{'nome':'arsenio', 'sobrenome': 'neto'},
-             {'nome':'slan', 'sobrenome': 'vieira'}]
+diconario = [
+    {"nome": "arsenio", "sobrenome": "neto"},
+    {"nome": "slan", "sobrenome": "vieira"},
+]
 
-diconario.sort(key=lambda item: item['sobrenome'])
+diconario.sort(key=lambda item: item["sobrenome"])
 
 for item in diconario:
     print(item)
+
+
+"""''
+
+lista = [1, 3, 3, 4, 5]
+soma = 0
+for c in lista:
+    soma += c
+print(soma)
+"""
