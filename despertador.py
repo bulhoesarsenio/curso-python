52264499671773c42c7bc8bdb3b508de55cdd4e3
Arsênio Bulhões <bulhoesarsenio@gmail.com>
2023-06-26 19:55:17 -0300

    despertador em python

--- despertador.py
+++ despertador.py
@@ -1,17 +1,32 @@
+lista_de_musica = ['loniest.mp3', 'medo.mp3']
+
 from time import localtime
 import pygame
 pygame.init()
-pygame.mixer.music.load('musica.mp3')
-pygame.mixer.music.play()
-input()
+escolha_musica = int(input(f'escolha a música pelo indice: {lista_de_musica}'))
+h = input('horas: ')
+m = input('minutos: ')
 
-pygame.event.wait() 
+while True:
+    if localtime().tm_hour == int(h) and localtime().tm_min == int(m):
+      
+        pygame.mixer.music.load(lista_de_musica[escolha_musica])
+        pygame.mixer.music.play()
+        input()
+
+
 ''''
-
 from time import localtime
 import pygame
+pygame.init()
 h = input('h: ')
 m = input('m: ')
 while True:
     if localtime().tm_hour == int(h) and localtime().tm_min == int(m):
+        pygame.mixer.music.load('loniest.mp3')
+        pygame.mixer.music.play()
+input()
+
+    
 '''
+
