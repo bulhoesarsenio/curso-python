dbb9c7a0ae490c640bfb5c3ed59a4f40f91b7af6
Alan Queiroz <alanqueiroz@outlook.com>
2025-01-26 15:01:46 -0300

    isso é um teste

--- exercicio_1.py
+++ exercicio_1.py
@@ -4,3 +4,11 @@ data_de_nascimento = 2008
 print('nome:', nome)
 print('idade:',idade)
 print('data de nascimentos:',data_de_nascimento)
+
+
+nome = 'arsênio'
+idade = 12
+data_de_nascimento = 2008
+print('nome:', nome)
+print('idade:',idade)
+print('data de nascimentos:',data_de_nascimento)
